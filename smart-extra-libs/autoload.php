983fe637d3603834ddf496a4acf8173ea1bd57ea
ilies.ovidiu@googlemail.com <ilies.ovidiu@googlemail.com>
2016-03-25 18:00:23 +0200

    Stable: RELEASE 2.3.1.6 / 2016-03-25 :: TAG FINAL / FIXED :: SVN.123

--- smart-extra-libs/autoload.php
+++ smart-extra-libs/autoload.php
@@ -13,7 +13,7 @@ require_once('modules/smart-extra-libs/version.php'); // extra libs version
 //--
 
 /**
- * Function AutoLoad Extra Libs from (Smart.Framework.Modules) r.160311
+ * Function AutoLoad Extra Libs from (Smart.Framework.Modules) r.160325
  * they are loaded via Dependency Injection
  *
  * @access 		private
