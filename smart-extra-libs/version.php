573787177373bbffe5a677aa99d591103056b35d
unix-world <iradu@unix-world.org>
2017-01-09 12:26:28 +0200

    Stable: RELEASE 2.3.7.5 / 2017-01-09 :: TAG FINAL svn.221

--- smart-extra-libs/version.php
+++ smart-extra-libs/version.php
@@ -1,7 +1,7 @@
 <?php
 // [APP - (Extra) Libs AutoLoad]
-// (c) 2006-2016 unix-world.org - all rights reserved
-// v.2.3.7.3 r.2016.10.05 / smart.framework.modules.v.2.3
+// (c) 2006-2017 unix-world.org - all rights reserved
+// v.2.3.7.5 r.2017.01.09 / smart.framework.v.2.3
 
 //----------------------------------------------------- PREVENT EXECUTION BEFORE RUNTIME READY
 if(!defined('SMART_FRAMEWORK_RUNTIME_READY')) { // this must be defined in the first line of the application
@@ -9,8 +9,8 @@ if(!defined('SMART_FRAMEWORK_RUNTIME_READY')) { // this must be defined in the f
 } //end if
 //-----------------------------------------------------
 
-define('SMART_FRAMEWORK_MODULES_HEAD_VERSION', 'v.2.3.7.3');
-define('SMART_FRAMEWORK_MODULES_VERSION', 'r.2016.10.05');
+define('SMART_FRAMEWORK_MODULES_HEAD_VERSION', 'v.2.3.7.5');
+define('SMART_FRAMEWORK_MODULES_VERSION', 'r.2017.01.09');
 
 if(((string)SMART_FRAMEWORK_RELEASE_TAGVERSION != (string)SMART_FRAMEWORK_MODULES_HEAD_VERSION) OR ((string)SMART_FRAMEWORK_MODULES_VERSION > (string)SMART_FRAMEWORK_RELEASE_VERSION)) { // check framework version
 	die('Smart.Framework.Modules requires Smart.Framework '.SMART_FRAMEWORK_MODULES_HEAD_VERSION.' '.SMART_FRAMEWORK_MODULES_VERSION.' ...');
