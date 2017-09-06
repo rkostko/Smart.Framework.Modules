59916f1d3e2ce0286b69f3b51b2d25ce6668186a
unix-world <iradu@unix-world.org>
2017-09-06 12:15:52 +0300

    TMP svn.741

---
+++ mod-sm-twitter/views/js/demo/setup.js
@@ -0,0 +1,9 @@
+
+// Twitter Api Setup
+
+var twitterConsumerKey = '';
+var twitterConsumerSecret = '';
+var twitterCbProxyUrl = '?/page/sm-twitter/codebird-proxy/';
+var twitterLocalCallbackUrl = 'callback.html';
+
+// END
