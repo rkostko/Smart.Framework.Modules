0ead04c01d6dae1ff76ce0e6988e3384139ea768
unix-world <iradu@unix-world.org>
2017-09-29 11:01:34 +0300

    Stable, svn.859

--- smart-extra-libs/version.php
+++ smart-extra-libs/version.php
@@ -10,11 +10,11 @@ if(!defined('SMART_FRAMEWORK_RUNTIME_READY')) { // this must be defined in the f
 //-----------------------------------------------------
 
 //-- defines the modules/extralibs version (required for info)
-define('SMART_APP_MODULES_EXTRALIBS_VER', 'm.ext.170928');
+define('SMART_APP_MODULES_EXTRALIBS_VER', 'm.ext.170929');
 //--
 
 //-- checks the minimum version of the Smart.Framework to run on
-define('SMART_APP_MODULES_EXTRALIBS_MIN_FRAMEWORK_VER', 'v.3.5.7.r.2017.09.28'); // this must be used to validate the required minimum framework version
+define('SMART_APP_MODULES_EXTRALIBS_MIN_FRAMEWORK_VER', 'v.3.5.7.r.2017.09.29'); // this must be used to validate the required minimum framework version
 if(version_compare((string)SMART_FRAMEWORK_RELEASE_TAGVERSION.(string)SMART_FRAMEWORK_RELEASE_VERSION, (string)SMART_APP_MODULES_EXTRALIBS_MIN_FRAMEWORK_VER) < 0) {
 	die('The Smart.Framework.Modules.ExtraLibs requires require the Smart.Framework '.SMART_APP_MODULES_EXTRALIBS_MIN_FRAMEWORK_VER.' or later !');
 } //end if
