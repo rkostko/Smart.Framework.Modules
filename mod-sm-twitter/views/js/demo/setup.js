7054618de141b0a5c9131371fd5af647ce027470
unix-world <iradu@unix-world.org>
2018-09-28 17:37:30 +0300

    Upgr SmTwitter

--- mod-sm-twitter/views/js/demo/setup.js
+++ mod-sm-twitter/views/js/demo/setup.js
@@ -1,8 +1,8 @@
 
 // Twitter Api Setup :: v.170919
 
-var twitterConsumerKey = '';
-var twitterConsumerSecret = '';
+var twitterConsumerKey = '81xfZwFKAPlGTfFxb526dmZnS';
+var twitterConsumerSecret = 'IUCaLbOm6RqPVZLOA07i27QhyE1zHeVlcZGOaMHwcT0Xfwrd9Y';
 var twitterCbProxyUrl = 'https://127.0.0.1/sites/frameworks/smart-framework/index.php/page/sm-twitter.codebird-proxy/~/';
 var twitterLocalCallbackUrl = 'https://127.0.0.1/sites/frameworks/smart-framework/modules/mod-sm-twitter/views/js/demo/callback.html';
 var twitterLocalBaseDomain = null; // null or domain.ext if multi sub-domain web app ...
